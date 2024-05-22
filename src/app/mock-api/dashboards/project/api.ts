05705af938b37406e9ea17b7d836666416a6d885
JERONIMO MAYORCA <mayorcajeronimo@gmail.com>
2024-05-21 23:15:20 -0500

    end home section of profile view

--- src/app/mock-api/dashboards/project/api.ts
+++ src/app/mock-api/dashboards/project/api.ts
@@ -7,49 +7,20 @@ import { cloneDeep } from 'lodash-es';
 export class ProjectMockApi
 {
     private _project: any = projectData;
-    private _defaultNavigation: any = []
-    private _compactNavigation:any = []
 
-    /**
-     * Constructor
-     */
+
     constructor(private _fuseMockApiService: FuseMockApiService)
     {
-        // Register Mock API handlers
         this.registerHandlers();
     }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Public methods
-    // -----------------------------------------------------------------------------------------------------
 
-    /**
-     * Register Mock API handlers
-     */
     registerHandlers(): void
     {
 
         this._fuseMockApiService
             .onGet('api/dashboards/project')
             .reply(() => [200, cloneDeep(this._project)]);
-
-        // Navigation - GET
-        this._fuseMockApiService
-            .onGet('api/navigation')
-            .reply(() => {
-
-            // Clone the data to preserve the originals
-            const compactNavigation = cloneDeep(this._compactNavigation);
-            const defaultNavigation = cloneDeep(this._defaultNavigation);
-
-            // Do some stuff with your data
-
-            // Return
-            return [200, {
-                compact: compactNavigation,
-                default: defaultNavigation,
-            }];
-        })
 
     }
 }
